3c58e4a42c4c4531909c83556760116e876fef5c
WFZ1 <pavelskumenko@gmail.com>
2021-04-26 12:21:39 +0300

    feat: do reset of filters

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,15 +1,28 @@
 (function() {
-  /* CHANGE FILTER ============================================================ */
+  const filters = document.querySelectorAll('input[type="range"]');
+  
+  /* ADJUSTMENT OF FILTERS FOR IMAGE ============================================================ */
 
-  const inputs = document.querySelectorAll('input[type="range"]');
-
-  const handleUpdate = (e) => {
+  const updateFilter = (e) => {
     const el = e.currentTarget;
-    const suffix = el.dataset.sizing || '';
-    const output = el.value + suffix;
-    document.documentElement.style.setProperty(`--${el.name}`, output);
-    el.nextElementSibling.value = output;
+    const suffix = el.dataset.sizing;
+    document.documentElement.style.setProperty(`--${el.name}`, el.value + suffix);
+    el.nextElementSibling.value = el.value;
   }
  
-  inputs.forEach(el => el.addEventListener('input', handleUpdate));
+  filters.forEach(el => el.addEventListener('input', updateFilter));
+
+  /* RESET FILTERS FOR IMAGE ============================================================ */
+
+  const reset = document.querySelector('.btn-reset');
+  
+  reset.addEventListener('click', () => {
+    filters.forEach(el => {
+      const suffix = el.dataset.sizing;
+      el.value = el.defaultValue;
+      document.documentElement.style.setProperty(`--${el.name}`, el.defaultValue + suffix);
+      el.nextElementSibling.value = el.defaultValue;
+    }); 
+  });
+  
 }());
