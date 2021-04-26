1e0d25508747d448bae1ea0d0e043a6486ebb825
WFZ1 <pavelskumenko@gmail.com>
2021-04-26 17:49:06 +0300

    feat: realize to changing images by click button 'Next picture'

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -14,9 +14,9 @@
 
   /* RESET FILTERS FOR IMAGE ============================================================ */
 
-  const reset = document.querySelector('.btn-reset');
+  const btnReset = document.querySelector('.btn-reset');
   
-  reset.addEventListener('click', () => {
+  btnReset.addEventListener('click', () => {
     filters.forEach(el => {
       const suffix = el.dataset.sizing;
       el.value = el.defaultValue;
@@ -24,5 +24,42 @@
       el.nextElementSibling.value = el.defaultValue;
     }); 
   });
+
+  /* NEXT PICTURE ============================================================ */
   
+  const btnNextImg = document.querySelector('.btn-next');
+  const image = {
+    tag: document.querySelector('img'),
+    baseUrl: 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/',
+    arr: [
+      '01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'
+    ],
+    pointer: 0
+  }
+
+  const loadImage = url => {
+    const img = new Image();
+    img.addEventListener('load', () => image.tag.src = url);
+    img.src = url;
+  };
+
+  const getImage = () => {
+    const index = image.pointer % image.arr.length;
+    const curTime = new Date().getHours();
+    let partOfDay = 'morning';
+
+    if (curTime >= 12 && curTime < 18) {
+      partOfDay = 'day';
+    } else if (curTime >= 18 && curTime < 24) {
+      partOfDay = 'evening';
+    } else if (curTime >= 0 && curTime < 6) {
+      partOfDay = 'night';
+    }
+
+    const url = image.baseUrl + partOfDay + '/' + image.arr[index];
+    loadImage(url);
+    image.pointer++;
+  }
+
+  btnNextImg.addEventListener('click', getImage);
 }());
